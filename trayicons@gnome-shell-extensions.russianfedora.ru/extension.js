6720ebc2bdd861c2cc0fdc6cb890ce89110fbbc6
Arkady L. Shane <ashejn@gmail.com>
2011-05-31 18:15:53 +0400

    added some tray icons

---
+++ trayicons@gnome-shell-extensions.russianfedora.ru/extension.js
@@ -0,0 +1,16 @@
+const Panel = imports.ui.panel;
+const StatusIconDispatcher = imports.ui.statusIconDispatcher;
+
+
+function main() {
+
+    // add the notification(s) you want display on the top bar
+    // - one per line. Use the english text string displayed when
+    // hovering your mouse over the bottom right notification area
+
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['gajim.py'] = 'gajim.py';  
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['empathy'] = 'empathy';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['pidgin'] = 'pidgin';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['abrt-applet'] = 'abrt-applet';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['seapplet'] = 'seapplet';
+}
